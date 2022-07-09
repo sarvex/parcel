827aa02aa561c05a8a8c9c064358ba33b1f34dc7
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2022-07-09 19:17:31 +0200

    WIP: less streams

--- packages/core/types/index.js
+++ packages/core/types/index.js
@@ -1671,13 +1671,20 @@ export type Optimizer<ConfigType, BundleConfigType> = {|
  */
 export type Compressor = {|
   compress({|
+    contents: Blob,
     stream: Readable,
     options: PluginOptions,
     logger: PluginLogger,
-  |}): Async<?{|
-    stream: Readable,
-    type?: string,
-  |}>,
+  |}): Async<
+    | ?{|
+        stream: Readable,
+        type?: string,
+      |}
+    | {|
+        contents: Blob,
+        type?: string,
+      |},
+  >,
 |};
 
 /**
